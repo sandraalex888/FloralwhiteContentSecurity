81a40ef07c65f51a14a80b1e9694b36b044b48f9
Oleksandra Aleksyeyenko <sandraalex888@gmail.com>
2020-12-16 18:56:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//for (let i = 0; i < 10; i++) {
+ // console.log(i);
+//}
+
+//for (let i = 100; i >= 0; i--) {
+// console.log(i);
+//}
+
+//let val = 0;
+//let otherVal = val++;
+//console.log(val, otherVal);
+
+//let i = 0;
+//let j = 0;
+//console.log(i++, ++j);
+//console.log(i,j);
+
+  //for (let i=100; i>=0; i-=2) { 
+    //console.log(i);
+  //}
+
+  // let i = 0;
+  // while (i <= 10) {
+  //  console.log(i);
+  //  i++;
+  // }
+
+  //let num = parseInt(prompt('Enter a number'));
+ // while(isNaN(num)) {
+ //   num = parseInt(prompt('Enter a number'))
+//}
+//console.log(num);
+
+// let num = 10;
+// do {
+//   num = parseInt(promt('Enter a number'));
+// } while (isNaN(num))
+// console.log(num);
+
+let products = ['Tesla Model 3', 
+                'Toyota Tunder',
+                 'Skoda Karoq'];
+//console.log(products.length);
+//console.log(products[4]);
+products[1] = 'Volkswagen Tuareg';
+products.push('Audi Q8');
+//products.push(prompt());
+console.log(products);
+
+// for (let i = 0; i < products.length; i++) {
+//   console.log(products[i]);
+// }
+
+// for (let i =products.length - 1; i >= 0; i--) {
+//   console.log(products[i]);
+// }
+
+// function prn(text) {
+//   console.log(text);
+// }
+// products.forEach(prn);
+
+// for (const product of products ) {
+//   console.log(product);
+// }
